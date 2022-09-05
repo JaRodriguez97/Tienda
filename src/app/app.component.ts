dc5dde67b6c5a4830a7fbb6ff0210cdcc4e150d3
JaRodriguez97 <cristi542012@gmail.com>
2022-09-05 17:26:54 -0500

    estructura y contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'tienda';
+  title = "tienda";
+  hidden: boolean;
+
+  constructor() {
+    this.hidden = false;
+  }
+
+  getTienda(): string {
+    this.hidden = true;
+    return "/productos";
+  }
 }
